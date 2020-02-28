9c2f501f2c2b7f2325f084c3704ff71aab43964f
Piyush Gamit <61284208+piyushgamit664@users.noreply.github.com>
2020-02-28 22:26:03 +0530

    Update demo.php

--- demo.php
+++ demo.php
@@ -6,23 +6,17 @@
 			$con=mysqli_connect("remotemysql.com","SsLhp88JCI","5SuauyG5hL","SsLhp88JCI") or 
 				die("dbnot connect");
 				
-				if ($con -> connect_errno) {
-					echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-				exit();
-					}else {
-						echo "Success";
-					}
 				
 		header('Access-Control-Allow-Origin: *');
 	    header('Access-Control-Allow-Headers: X-Requested-With');
 	    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 		
-		$data= file_get_contents("php://input");
-		$json_obj = json_decode($data);
-		$UserId=$json_obj->UserId;
+		//$data= file_get_contents("php://input");
+		//$json_obj = json_decode($data);
+		//$UserId=$json_obj->UserId;
 		
 		$responce['rec']=array();
-		$q="select CategoryId,CategoryName,CreatedOn from category where UserId='".$UserId."'";
+		$q="select CategoryId,CategoryName,CreatedOn from category";
 		$res=mysqli_query($con,$q);
 		while($row=mysqli_fetch_array($res)){
 			array_push($responce['rec'],$row);
