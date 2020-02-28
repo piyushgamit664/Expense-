cf2fc93bd8180b5ed22bae36f0a24768efeacc10
Piyush Gamit <61284208+piyushgamit664@users.noreply.github.com>
2020-02-28 22:28:47 +0530

    Update demo.php

--- demo.php
+++ demo.php
@@ -11,12 +11,12 @@
 	    header('Access-Control-Allow-Headers: X-Requested-With');
 	    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 		
-		//$data= file_get_contents("php://input");
-		//$json_obj = json_decode($data);
-		//$UserId=$json_obj->UserId;
+		$data= file_get_contents("php://input");
+		$json_obj = json_decode($data);
+		$UserId=$json_obj->UserId;
 		
 		$responce['rec']=array();
-		$q="select CategoryId,CategoryName,CreatedOn from category";
+		$q="select CategoryId,CategoryName,CreatedOn from category where UserId='".$UserId."'";
 		$res=mysqli_query($con,$q);
 		while($row=mysqli_fetch_array($res)){
 			array_push($responce['rec'],$row);
