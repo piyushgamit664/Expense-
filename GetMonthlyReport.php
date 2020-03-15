eaf89d7fbb0971b1b93d0616fa18ef0e2a0204d6
Piyush Gamit <61284208+piyushgamit664@users.noreply.github.com>
2020-03-15 10:25:13 +0530

    report

---
+++ GetMonthlyReport.php
@@ -0,0 +1,31 @@
+ 	<?php
+		$responce=array();
+		//$con=mysqli_connect("localhost","root","","Expense") or 
+				//die("dbnot connect");
+			
+			$con=mysqli_connect("remotemysql.com","SsLhp88JCI","5SuauyG5hL","SsLhp88JCI") or 
+				die("dbnot connect");
+					
+				
+		header('Access-Control-Allow-Origin: *');
+	    header('Access-Control-Allow-Headers: X-Requested-With');
+	    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+		
+		$data= file_get_contents("php://input");
+		$json_obj = json_decode($data);
+		$UserId=$json_obj->UserId;
+		
+		$responce['rec']=array();
+		$q="SELECT YEAR(Date) as Year, MONTH(Date) as Month, sum(Amount) as TotalAmount
+			FROM
+			incomeexpense
+			where UserId='".$UserId."'
+			GROUP BY YEAR(Date), MONTH(Date) DESC";
+			
+		$res=mysqli_query($con,$q);
+		while($row=mysqli_fetch_array($res)){
+			array_push($responce['rec'],$row);
+		}
+		 echo	json_encode($responce);
+			
+?>
